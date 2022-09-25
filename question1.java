9e76bf769775562dd6cd8796638504c897995a18
akj762662 <80040264+ajimproviser@users.noreply.github.com>
2022-09-26 00:33:43 +0530

    Add files via upload

---
+++ question1.java
@@ -0,0 +1,8 @@
+public class question1{
+    public static void main(String args[]){
+        double score = 81.79d;
+        if(score>=80 && score<=90){
+            System.out.println(score+5);
+        }
+    }
+}
---
+++ question6.java
@@ -0,0 +1,10 @@
+public class question6 {
+    public static void main(String args[]){
+        int a=3;
+        do{
+            System.out.print(a+" ");
+            a+=3;
+        }
+        while(a<=36);
+    }
+}
